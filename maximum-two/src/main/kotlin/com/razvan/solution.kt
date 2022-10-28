2c553c90f9b77a69b55e1d4618dd97ef0c68ac12
Răzvan Filip <razvandfilip@gmail.com>
2022-10-28 15:36:32 +0300

    More explicit solution

--- maximum-two/src/main/kotlin/com/razvan/solution.kt
+++ maximum-two/src/main/kotlin/com/razvan/solution.kt
@@ -1,6 +1,82 @@
 package com.razvan
 
+
 fun maxTwo(array: IntArray): Pair<Int, Int> {
+    var imax: Int? = null
+    var imax2: Int? = null
+    for (i in array.indices) {
+        if (array[i] >= 0) {
+            if (imax == null) {
+                imax = i
+            } else if(array[i] > array[imax]) {
+                imax = i
+            }
+        }
+    }
+    if (imax != null) {
+        for (i in array.indices) {
+            if (array[i] >= 0 && i != imax) {
+                if (imax2 == null) {
+                    imax2 = i
+                } else if(array[i] > array[imax2]) {
+                    imax2 = i
+                }
+            }
+        }
+    }
+
+    var imin: Int? = null
+    var imin2: Int? = null
+    for (i in array.indices) {
+        if (array[i] <= 0) {
+            if (imin == null) {
+                imin = i
+            } else if(array[i] < array[imin]) {
+                imin = i
+            }
+        }
+    }
+    if (imin != null) {
+        for (i in array.indices) {
+            if (array[i] <= 0 && i != imin) {
+                if (imin2 == null) {
+                    imin2 = i
+                } else if(array[i] < array[imin2]) {
+                    imin2 = i
+                }
+            }
+        }
+    }
+
+    val maxProduct = product(imax?.let { array[it] }, imax2?.let { array[it] })
+    val minProduct = product(imin?.let { array[it] }, imin2?.let { array[it] })
+
+    if (maxProduct != null && minProduct != null) {
+        if (maxProduct > minProduct) {
+            return Pair(array[imax!!], array[imax2!!])
+        } else {
+            return Pair(array[imin!!], array[imin2!!])
+        }
+    } else {
+        if (maxProduct != null) {
+            return Pair(array[imax!!], array[imax2!!])
+        } else if (minProduct != null) {
+            return Pair(array[imin!!], array[imin2!!])
+        } else {
+            return Pair(array[imax!!], array[imin!!])
+        }
+    }
+}
+
+fun product(one: Int?, two: Int?): Int? {
+    if (one == null || two == null) {
+        return null
+    } else {
+        return one * two
+    }
+}
+
+fun maxTwo2(array: IntArray): Pair<Int, Int> {
     var iFirstMax: Int
     var iSecondMax: Int
     var iFirstMin: Int
