962aa99e28fc2d9d10ac4e137f224dcf6738a461
Răzvan Filip <razvandfilip@gmail.com>
2022-11-09 17:10:42 +0200

    Add weighted version

--- graph-implementation/src/main/kotlin/com/razvan/solution.kt
+++ graph-implementation/src/main/kotlin/com/razvan/solution.kt
@@ -2,13 +2,40 @@ package com.razvan
 
 import kotlin.math.max
 
+class WeightedGraph(
+    edges: List<WeightedEdge>
+) {
+    private val adjacencyList: List<List<Vec>>
+
+    init {
+        val maxVertex = edges.fold(0) {
+                max, edge -> max(max, max(edge.start, edge.end))
+        }
+
+        val mutableList = mutableListOf<MutableList<Vec>>()
+        for (i in 0..maxVertex) {
+            mutableList.add(mutableListOf())
+        }
+
+        edges.forEach { (start, end, weight) ->
+            (mutableList[start].add(Vec(end, weight)))
+        }
+        adjacencyList = mutableList
+    }
+
+    override fun toString(): String {
+        return adjacencyList.mapIndexed { start, list ->
+            list.joinToString(separator = "    ") { end -> "($start --> ${end.dest} (${end.weight}))" }
+        }.joinToString(separator = "\n")
+    }
+}
+
 class Graph(
-    edges: List<Edge>,
+    edges: List<Edge>
 ) {
     private val adjacencyList: List<List<Int>>
 
     init {
-
         val maxVertex = edges.fold(0) {
             max, edge -> max(max, max(edge.start, edge.end))
         }
@@ -33,7 +60,17 @@ class Graph(
 
 data class Edge(
     val start: Int,
-    val end: Int,
+    val end: Int
 )
 
+data class WeightedEdge(
+    val start: Int,
+    val end: Int,
+    val weight: Int
+)
+
+data class Vec(
+    val dest: Int,
+    val weight: Int
+)
 
--- graph-implementation/src/test/kotlin/com/razvan/GraphTest.kt
+++ graph-implementation/src/test/kotlin/com/razvan/GraphTest.kt
@@ -12,14 +12,40 @@ class GraphTest {
             Edge(0, 1), Edge(1, 2), Edge(2, 0), Edge(2, 1), Edge(3, 2), Edge(4, 5), Edge(5, 4)
         )
         val graph = Graph(edges)
-        assertThat(graph.toString()).isEqualTo("""
+        assertThat(graph.toString()).isEqualTo(
+            """
             (0 --> 1)
             (1 --> 2)
             (2 --> 0)    (2 --> 1)
             (3 --> 2)
             (4 --> 5)
             (5 --> 4)
-        """.trimIndent())
+        """.trimIndent()
+        )
+    }
 
+    @Test
+    fun testWeightedDigraphAdjacency() {
+
+        val edges = listOf(
+            WeightedEdge(0, 1, 6),
+            WeightedEdge(1, 2, 7),
+            WeightedEdge(2, 0, 5),
+            WeightedEdge(2, 1, 4),
+            WeightedEdge(3, 2, 10),
+            WeightedEdge(4, 5, 1),
+            WeightedEdge(5, 4, 3)
+        )
+        val graph = WeightedGraph(edges)
+        assertThat(graph.toString()).isEqualTo(
+            """
+            (0 --> 1 (6))
+            (1 --> 2 (7))
+            (2 --> 0 (5))    (2 --> 1 (4))
+            (3 --> 2 (10))
+            (4 --> 5 (1))
+            (5 --> 4 (3))
+        """.trimIndent()
+        )
     }
 }
